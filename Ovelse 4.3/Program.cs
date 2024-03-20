d27c460882725cbd255c7eae612ccc1a13183c6b
Christoffer Olsen <chris70944@gmail.com>
2024-03-20 22:00:56 +0100

    Done

--- Ovelse 4.3/Program.cs
+++ Ovelse 4.3/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Vil du have ananas på din pizza?");
+
+string svar = Console.ReadLine();
+
+if (svar == ja)
+{
+    Console.WriteLine("Det skal du få.");
+}
